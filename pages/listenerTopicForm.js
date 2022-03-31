d81e17633bf59b0ae6861c5471d45c59c4c95fe0
madoka nagai <partner@propo.fm>
2022-03-31 14:14:34 +0900

    トピック登録画面作成

--- pages/listenerTopicForm.js
+++ pages/listenerTopicForm.js
@@ -13,12 +13,32 @@ export default function ListenerTopicForm () {
     handleSubmit,
     watch,
     getValues,
-    formState: { errors }
+    formState: { errors },
+    // defaultValues
   } = useForm();
 
   const router = useRouter();
+
   const password = useRef({});
   password.current = watch("password", "");
+
+  const topicLists = [
+    "テック",
+    "科学",
+    "文化",
+    "ニュース",
+    "マーケティング",
+    "書籍",
+    "ビジネス",
+    "アート",
+    "政治",
+    "金融",
+    "生産性",
+    "ライフスタイル",
+    "文学",
+    "哲学",
+    "内省",
+  ]
 
   const onSubmit = async (data) => {
     console.log(data)
@@ -37,7 +57,6 @@ export default function ListenerTopicForm () {
     if (result.status === 'success') {
       router.push("/topicComplete")
     }
-
   }
 
 
@@ -49,131 +68,83 @@ export default function ListenerTopicForm () {
         興味があるトピックを教えていただけると、よりお好みに近い内容をお届けすることができます。<br />
         ぜひご回答ください。
         </p>
-        <div className={`${styles.c_flexForm_area} ${styles.flexstart}`}>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />テック
-            </label>
+        <form className={`${styles.c_normalForm_area} ${styles.mb30}`} onSubmit={handleSubmit(onSubmit)}>
+          <div className={`${styles.c_flexForm_area} ${styles.flexstart} ${styles.bborder} ${styles.mb24}`}>
+
+            { topicLists.map((item, index) => {
+              index = index + 1
+              return (
+                <div className={styles.c_check_wrap} key={`key_${index}`}>
+                <label className={styles.c_label_check}>
+                  <input 
+                    {...register("favoriteTopics[]", {})}
+                    id="topics"
+                    type="checkbox"
+                    value={item}
+                    name="favoriteTopics[]"
+                    className={styles.c_input_check} 
+                    />
+                    {item}
+                </label>
+              </div>
+              )
+            }) }
           </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />科学
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />文化
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />ニュース
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />マーケティング
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />書籍
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />ビジネス
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />アート
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />政治
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />金融
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />生産性
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />ライフスタイル
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />文学
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />哲学
-            </label>
-          </div>
-          <div className={styles.c_check_wrap}>
-            <label className={styles.c_label_check}>
-              <input className={styles.c_input_check} type="checkbox" />内省
-            </label>
-          </div>
-        </div>
-      </section>
-      <section className={styles.passwd_resister}>
-      <p className={`${styles.c_text} ${styles.mb24}`}>
-        PROPO.FM のパスワードを設定してください。
-      </p>
-      <form className={`${styles.c_normalForm_area} ${styles.mb30}`} onSubmit={handleSubmit(onSubmit)}>
-        <div className={`${styles.c_form_title}`}>パスワード<span>*必須</span></div>
-        <div className={`${styles.c_input_wrap_grey} ${styles.email_form_wrap} ${styles.mb24}`}>
-          <input 
-            id="password"
-            type="text"
-            name="password"
-            placeholder="半角英数字6文字以上"
-            {...register("password", { 
-              required: true ,
-              minLength: {
-                value: 6,
-                message: "",
-              },
-            })}
-            className={`${styles.c_input_text_grey} ${styles.email_form}`}  />
-            {errors.password?.type === "required" && <p className={styles.c_error_text}>パスワードを入力してください</p>}
-            {errors.password?.type === "minLength" && <p className={styles.c_error_text}>パスワードは6文字以上で入力してください</p>}
-          </div>
+          <div className={styles.passwd_resister}>
+          <p className={`${styles.c_text} ${styles.mb24}`}>
+            PROPO.FM のパスワードを設定してください。
+          </p>
           <div className={`${styles.c_form_title}`}>パスワード<span>*必須</span></div>
-          <div className={`${styles.c_input_wrap_grey} ${styles.email_form_wrap} ${styles.mb40}`}>
+          <div className={`${styles.c_input_wrap_grey} ${styles.email_form_wrap} ${styles.mb24}`}>
             <input 
-              id="passwordRepeat"
-              type="text"
-              name="passwordRepeat"
+              id="password"
+              type="password"
+              name="password"
               placeholder="半角英数字6文字以上"
-              // {...register("passwordRepeat")}
-              {...register("passwordRepeat", {
-                validate: value => value === password.current,
-              })} 
-              className={`${styles.c_input_text_grey} ${styles.email_form}`} />
-            <img className={`${styles.c_ico_eye} ${styles.eye_off}`} src="/images/ico_eye_off.svg" />
-            {errors.passwordRepeat?.type === "validate" && <p className={styles.c_error_text}>入力したパスワードと一致していません</p>}
-          </div>
-          <div className={`${styles.c_roundBtn} ${styles.mb24} ${styles.align_center_pc}`}>
-  					<a href="https://propo.fm/" className={styles.topChannel__btn__link}>
-              <span>この内容を保存する</span>
-              <img className={styles.ico_right} src="/images/ico_check.svg" />
-            </a>
-            <input className={styles.c_submit_btn_hidden} type="submit" value="" />
-				  </div>
-        </form>
+              {...register("password", { 
+                required: { value: true , message: "パスワードを入力してください" },
+                minLength: {
+                  value: 6,
+                  message: "パスワードは6文字以上で入力してください",
+                },
+              })}
+              className={`${styles.c_input_text_grey} ${styles.email_form}`}  />
+              <img className={`${styles.c_ico_eye} ${styles.eye_off}`} src="/images/ico_eye_off.svg" />
+
+              {errors.password && (
+                <p className={styles.c_error_text}>{errors.password.message}</p>
+              )}
+            </div>
+            <div className={`${styles.c_form_title}`}>パスワード<span>*必須</span></div>
+            <div className={`${styles.c_input_wrap_grey} ${styles.email_form_wrap} ${styles.mb40}`}>
+              <input 
+                id="passwordRepeat"
+                type="password"
+                name="passwordRepeat"
+                placeholder="半角英数字6文字以上"
+                {...register("passwordRepeat", {
+                  validate: value => 
+                    value === password.current || "入力したパスワードと一致していません" ,
+                })} 
+                className={`${styles.c_input_text_grey} ${styles.email_form}`} />
+              <img className={`${styles.c_ico_eye} ${styles.eye_off}`} src="/images/ico_eye_off.svg" />
+                              
+              {errors.passwordRepeat && (
+                <p className={styles.c_error_text}>{errors.passwordRepeat.message}</p>
+              )}
+            </div>
+
+            <div className={`${styles.c_roundBtn} ${styles.mb24} ${styles.align_center_pc}`}>
+              <a href="https://propo.fm/" className={styles.topChannel__btn__link}>
+                <span>この内容を保存する</span>
+                <img className={styles.ico_right} src="/images/ico_check.svg" />
+              </a>
+              <input className={styles.c_submit_btn_hidden} type="submit" value="" />
+            </div>
+        </div>
+          </form>
       </section>
+
       <CommonBox3 />
     </Layout>
   )
